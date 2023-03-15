f2c88187cd560b18a0f8847a67934653d5332c75
coconut <1424392205@qq.com>
2023-03-15 15:38:47 +0800

    feat: update yml

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 const { remarkCodeHike } = require("@code-hike/mdx");
 const theme = require("shiki/themes/dark-plus.json");
-
+const nextConfig = {
+  assetPrefix: "/build-your-own-react/",
+  images: {
+    unoptimized: true,
+  },
+};
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [[remarkCodeHike, { theme }]],
   },
+  ...nextConfig,
 });
 
 module.exports = withMDX({
